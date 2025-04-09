4c9bd9fd6444b556d9c56fccae043985b8924f8e
Sergey E. Koposov <skoposov@ed.ac.uk>
2025-04-09 21:54:48 +0100

    update

--- scripts/make_feh_distr.py
+++ scripts/make_feh_distr.py
@@ -9,40 +9,54 @@ fname = data_path + '/' + main_file
 
 pp.run()
 
-RV_T = atpy.Table().read(fname, 'RVTAB', mask_invalid=False)
-SP_T = atpy.Table().read(fname, 'SPTAB', mask_invalid=False)
-FM_T = atpy.Table().read(fname, 'FIBERMAP', mask_invalid=False)
-G_T = atpy.Table().read(fname, 'GAIA', mask_invalid=False)
-
-main_sel = (RV_T['RVS_WARN'] == 0) & (RV_T['RR_SPECTYPE'] == 'STAR')
-
-plt.clf()
-cnt = 0
-cur_sel0 = main_sel & (RV_T['SURVEY'] == 'main') & (
-    RV_T['PROGRAM'] == 'bright') & (RV_T['SN_R'] > 10)
-cur_sel0 = main_sel & (RV_T['PRIMARY']) & (RV_T['SN_R'] > 10)
-
 
 def betw(x, x1, x2):
     return (x >= x1) & (x < x2)
 
 
+RV_T = atpy.Table().read(fname, 'RVTAB', mask_invalid=False)
+SP_T = atpy.Table().read(fname, 'SPTAB', mask_invalid=False)
+FM_T = atpy.Table().read(fname, 'FIBERMAP', mask_invalid=False)
+G_T = atpy.Table().read(fname, 'GAIA', mask_invalid=False)
+
+main_sel = (RV_T['RVS_WARN'] == 0) & (RV_T['RR_SPECTYPE']
+                                      == 'STAR') & (RV_T['SN_R'] > 10)
+
+plt.clf()
+cnt = 0
+rv_sel = betw(RV_T['TEFF'], 4500, 7000) & (RV_T['VSINI'] < 30)
+sp_sel = (SP_T['BESTGRID'] != 's_rdesi1') & betw(SP_T['TEFF'], 4500, 7000)
+
+cur_sel0 = main_sel & RV_T['PRIMARY'] & (RV_T['SURVEY'] == 'main')
+#& (RV_T['PROGRAM']
+#                                                    == 'bright')
+
 fig = plt.figure(figsize=(3.37 * 1, 3.37 * .75))
 
-cur_sel = cur_sel0 & betw(RV_T['TEFF'], 4500, 7000) & (RV_T['VSINI'] < 30)
-opt = dict(alpha=.5, bins=100, range=[-5, .5], histtype='step')
+cur_sel = cur_sel0 & rv_sel
+opt = dict(alpha=.5, bins=55 * 2, range=[-5, .5], histtype='step')
 plt.hist(RV_T['FEH'][cur_sel], label='RVS', **opt)
-print('RV', (cur_sel & (RV_T['FEH'] < -2)).sum())
-print('RV', (cur_sel & (RV_T['FEH'] < -3)).sum())
-
-cur_sel = cur_sel0 & (SP_T['BESTGRID'] != 's_rdesi1') & betw(
-    SP_T['TEFF'], 4500, 7000)
+cur_sel = cur_sel0 & sp_sel
 plt.hist(SP_T['FEH'][cur_sel], label='SP', **opt)
-print('SP', (cur_sel & (SP_T['FEH'] < -2)).sum())
-print('SP', (cur_sel & (SP_T['FEH'] < -3)).sum())
 
+plt.plot([-1.4, -1.4], [100, 3e3], linestyle=':', color='grey')
+plt.plot([-2.1, -2.1], [100, 3e3], linestyle=':', color='grey')
 plt.gca().set_yscale('log')
 plt.xlabel('[Fe/H]')
 plt.legend()
 plt.tight_layout()
 plt.savefig('plots/feh_distr.pdf')
+
+print('main/bright, then all')
+for cur_sel0 in [
+        main_sel & (RV_T['SURVEY'] == 'main') & (RV_T['PROGRAM'] == 'bright'),
+        main_sel & RV_T['PRIMARY']
+]:
+    cur_sel = cur_sel0 & rv_sel
+    print('RV', (cur_sel & (RV_T['FEH'] < -2)).sum())
+    print('RV', (cur_sel & (RV_T['FEH'] < -3)).sum())
+
+    cur_sel = cur_sel0 & sp_sel
+    plt.hist(SP_T['FEH'][cur_sel], label='SP', **opt)
+    print('SP', (cur_sel & (SP_T['FEH'] < -2)).sum())
+    print('SP', (cur_sel & (SP_T['FEH'] < -3)).sum())

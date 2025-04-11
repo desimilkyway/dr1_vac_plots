792c53ee8a0789f67639061cab223a391e9f456e
Sergey E. Koposov <skoposov@ed.ac.uk>
2025-04-11 23:46:22 +0100

    add alphafe panels

--- scripts/make_feh_alpha.py
+++ scripts/make_feh_alpha.py
@@ -17,7 +17,7 @@ G_T = atpy.Table().read(fname, 'GAIA', mask_invalid=False)
 main_sel = (RV_T['RVS_WARN'] == 0) & (RV_T['RR_SPECTYPE'] == 'STAR')
 
 plt.clf()
-fig = plt.figure(figsize=(3.37 * 1, 3.37 * .55))
+fig = plt.figure(figsize=(3.37 * 1, 3.37 * .85))
 cnt = 0
 cnt = 0
 cur_sel0 = (
@@ -30,43 +30,51 @@ def betw(x, x1, x2):
     return (x >= x1) & (x < x2)
 
 
-gs = gridspec.GridSpec(nrows=1,
+gs = gridspec.GridSpec(nrows=2,
                        ncols=3,
                        width_ratios=[1, 1, 0.05],
-                       wspace=0.05)
-for cnt in range(2):
-    T = [RV_T, SP_T][cnt]
-    if cnt == 0:
-        cur_sel = cur_sel0 & (RV_T['VSINI'] < 30)
-    else:
-        cur_sel = cur_sel0 & (SP_T['BESTGRID'] != 's_rdesi1')
-    cur_sel = cur_sel & betw(T['TEFF'], 4500, 7000)
-    # plt.subplot(1, 2, cnt + 1)
-    plt.subplot(gs[cnt])
-    im = plt.hist2d((T['FEH'][cur_sel]),
-                    T['ALPHAFE'][cur_sel],
-                    range=[[-5, 1], [-.5, 1.3]],
-                    bins=[100, 100],
-                    norm=maco.PowerNorm(gamma=.5, vmax=10000))
+                       wspace=0.05,
+                       hspace=.13,
+                       bottom=.1)
+for xcnt in range(2):
+    for cnt in range(2):
+        T = [RV_T, SP_T][cnt]
+        if cnt == 0:
+            cur_sel = cur_sel0 & (RV_T['VSINI'] < 30)
+        else:
+            cur_sel = cur_sel0 & (SP_T['BESTGRID'] != 's_rdesi1')
+        cur_sel = cur_sel & betw(T['TEFF'], 4500, 7000)
+        # plt.subplot(1, 2, cnt + 1)
+        plt.subplot(gs[xcnt, cnt])
+        rr = {0: [[-5, 1], [-.5, 1.3]], 1: [[-2, .5], [-.2, .7]]}[xcnt]
+        vmax = {0: 10000, 1: 3000}[xcnt]
+        im = plt.hist2d((T['FEH'][cur_sel]),
+                        T['ALPHAFE'][cur_sel],
+                        range=rr,
+                        bins=[100, 100],
+                        norm=maco.PowerNorm(gamma=.5, vmax=vmax))
 
-    plt.gci().set_rasterized(True)
-    plt.xlabel('[Fe/H] ')
-    for xx in 'xy':
-        plt.gca().tick_params(axis=xx,
-                              which='both',
-                              labelcolor='black',
-                              color='white')
-    for xx in plt.gca().spines.keys():
-        plt.gca().spines[xx].set_color("white")
+        plt.gci().set_rasterized(True)
+        if xcnt == 1:
+            plt.xlabel('[Fe/H] ')
+        for xx in 'xy':
+            plt.gca().tick_params(axis=xx,
+                                  which='both',
+                                  labelcolor='black',
+                                  color='white')
+        for xx in plt.gca().spines.keys():
+            plt.gca().spines[xx].set_color("white")
+        plt.gca().tick_params(top=False, which='both')
 
-    # plt.title(['RVS', 'SP'][cnt])
-    plt.text(-2, 1.21 - .1, ['RVS', 'SP'][cnt], color='white')
-    if cnt == 0:
-        plt.ylabel(r'[$\alpha$/Fe]')
-    else:
-        plt.gca().yaxis.set_major_formatter(plt.NullFormatter())
-ax = plt.subplot(gs[2])
-plt.colorbar(im[-1], cax=ax)
+        # plt.title(['RVS', 'SP'][cnt])
+        if xcnt == 0:
+            plt.text(-2, 1.21 - .1, ['RVS', 'SP'][cnt], color='white')
+        if cnt == 0:
+            plt.ylabel(r'[$\alpha$/Fe]')
+        else:
+            plt.gca().yaxis.set_major_formatter(plt.NullFormatter())
+    ax = plt.subplot(gs[xcnt, 2])
+    plt.colorbar(im[-1], cax=ax)
 
 # plt.tight_layout()
 plt.subplots_adjust(wspace=0., hspace=0.001, top=.97, bottom=.16)
